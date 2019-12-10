a216053e2c65e7e2adcbabbf0919dd7f9b771f19
DEREKDUNES <mbahderek@gmail.com>
2019-12-10 15:15:58 +0100

    made some corrections on my desktop dev

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ new Vue({
 		name : 'Derek',
 		job : 'Ninja',
 		website: 'www.thenetninja.co.uk',
-		webTag: '<a href="www.thenetninja.co.uk">The Net Ninja Web</a>
+		webTag: '<a href="www.thenetninja.co.uk">The Net Ninja Web</a>'
 	},
 	methods : {
 		greet : function(time){
