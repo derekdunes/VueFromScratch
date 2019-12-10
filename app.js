8721cc89fd10bc0441c45884995f6560e2bde747
tobex001 <mbahtobex@gmail.com>
2019-12-10 06:43:51 +0100

    data, method and view binding

---
+++ app.js
@@ -0,0 +1,14 @@
+new Vue({
+	el: '#vue-app',
+	data: {
+		name : 'Derek',
+		job : 'Ninja',
+		website: 'www.thenetninja.co.uk',
+		webTag: '<a href="www.thenetninja.co.uk">The Net Ninja Web</a>
+	},
+	methods : {
+		greet : function(time){
+			return 'Good ' + time + ' ' + this.name;
+		}
+	}
+})
